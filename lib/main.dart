a413d1b7fea1f19b2585fc9f46804893cfd2678e
dalbodeule <11470513+dalbodeule@users.noreply.github.com>
2023-11-29 19:11:01 +0900

    asdf

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,82 @@
 // ignore_for_file: avoid_print, no_leading_underscores_for_local_identifiers
 
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get.dart';
-import 'package:soynote/controllers/app_controller.dart';
 
 import 'package:flutter/material.dart';
+import 'package:soynote/firebase_options.dart';
 import 'package:soynote/page/home.dart';
 
+const primaryColor = Colors.lightGreen;
+
 void main() async {
+  await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // ignore: unused_local_variable
+  FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  // ignore: unused_local_variable
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true
+  );
+
+  // ignore: unused_local_variable
+  const AndroidNotificationChannel androidNotificationChannel = AndroidNotificationChannel(
+    "soynote_channel", "soynote notification",
+    importance: Importance.max
+  );
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  await flutterLocalNotificationsPlugin.initialize(
+    const InitializationSettings(
+      android: AndroidInitializationSettings("@mipmap/ic_launcher"), iOS: DarwinInitializationSettings()
+    )
+  );
+
+  final fcmToken = await FirebaseMessaging.instance.getToken(vapidKey: 'BPcv5jKHuUvatdSnyldzC-JQqGNzwgweMSrtxoC4FmEQ3rZ-gW7Scatk9HPT9oCII1xnh8R-pTmGe5er17qgbOk');
+  
+  print("$fcmToken");
+  
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage rm) {
+    RemoteNotification? notification = rm.notification;
+    AndroidNotification? android = rm.notification?.android;
+
+    if (notification != null && android != null) {
+      flutterLocalNotificationsPlugin.show(
+        0,
+        notification.title,
+        notification.body,
+        const NotificationDetails(
+          android: AndroidNotificationDetails(
+            'soynote_channel', 
+            'soynote alert channel',
+          )
+        )
+      );
+    }
+
+  });
+
   WidgetsFlutterBinding.ensureInitialized();
 
   @pragma('vm:entry-point')
@@ -20,55 +88,24 @@ void main() async {
     print("Handling a background message: ${message.messageId}");
   }
 
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 // ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
-  final AppController c = Get.put(AppController());
-
+  const MyApp({Key? key}) : super(key: key);
+  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: "soynote",
-      home: Scaffold(
-        appBar: AppBar(),
-        body: FutureBuilder(
-          future: c.initialize(),
-          builder: (context, snapshot) {
-            if(snapshot.hasData && snapshot.connectionState == ConnectionState.done) {
-              return const HomePage(title: 'Soynote');
-            } else if(snapshot.hasError) {
-              return const Center(child: Text("Failed to initialize"));
-            } else {
-              return const Center(child: Text("initializing..."));
-            }
-          }
-        )
+      theme: ThemeData(
+        primaryColor: primaryColor
       ),
-      onGenerateRoute: (RouteSettings settings) {
-          switch(settings.name) {
-            case '/home':
-            case '/':
-              return MyCustomRoute(
-                builder: (_) => const HomePage(title: 'Soynote'),
-                settings: settings,
-              );
-          }
-          return null;
-      },
+      initialRoute: '/',
+      getPages: [
+        GetPage(name: '/', page: () => const HomePage())
+      ],
     );
-  }
-}
-
-class MyCustomRoute<T> extends MaterialPageRoute<T> {
-  MyCustomRoute({required WidgetBuilder builder, required RouteSettings settings})
-    : super(builder: builder, settings: settings);
-
-  @override
-  Widget buildTransitions(BuildContext context, Animation<double> animation,
-      Animation<double> secondaryAnimation, Widget child) {
-    return FadeTransition(opacity: animation, child: child);
   }
 }
--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:soynote/main.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<StatefulWidget> createState() {
-    return HomePageState();
-  }
-}
-
-class HomePageState extends State<HomePage> {
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -21,9 +13,29 @@ class HomePageState extends State<HomePage> {
           "Soynote",
           textAlign: TextAlign.center,
           style: TextStyle(fontSize: 20, color: Colors.black)
+        ),
+        backgroundColor: primaryColor
+      ),
+      drawer: Drawer(
+        backgroundColor: primaryColor,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(color: Colors.greenAccent),
+              child: Text('Menus')
+            ),
+            ListTile(
+              title: const Text('home'),
+              // ignore: unnecessary_const
+              onTap: () {
+                Get.toNamed('/');
+              }
+            )
+          ],
         )
       ),
-      body: Container(),
+      body: Container()
     );
   }
 }

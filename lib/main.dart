4529b9e034a99ed57e251559eaf48a9b74143b11
dalbodeule <11470513+dalbodeule@users.noreply.github.com>
2023-11-29 11:58:34 +0900

    upgrade flutter & add firebase message

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,60 @@
+// ignore_for_file: avoid_print, no_leading_underscores_for_local_identifiers
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'firebase_options.dart';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_bootstrap5/flutter_bootstrap5.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:soynote/page/home.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // ignore: unused_local_variable
+  FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  // ignore: unused_local_variable
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print("$fcmToken");
+
+  @pragma('vm:entry-point')
+  Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+    // If you're going to use other Firebase services in the background, such as Firestore,
+    // make sure you call `initializeApp` before using other Firebase services.
+    await Firebase.initializeApp();
+
+    print("Handling a background message: ${message.messageId}");
+  }
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
+
   runApp(MyApp());
 }
 
+// ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -22,6 +70,7 @@ class MyApp extends StatelessWidget {
                 settings: settings,
               );
           }
+          return null;
       },
     );
   }
---
+++ web/firebase-messaging-sw.js
@@ -0,0 +1,19 @@
+importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA553np3TvR19VDqij2I1WdXHT9I8nm1Kg",
+  authDomain: "soynote-aad8d.firebaseapp.com",
+  projectId: "soynote-aad8d",
+  storageBucket: "soynote-aad8d.appspot.com",
+  messagingSenderId: "1021708879332",
+  appId: "1:1021708879332:web:3fa410583b46dd9aba73f1",
+  measurementId: "G-M91YK32LPL"
+});
+// Necessary to receive background messages:
+const messaging = firebase.messaging();
+
+// Optional:
+messaging.onBackgroundMessage((m) => {
+  console.log("onBackgroundMessage", m);
+});
